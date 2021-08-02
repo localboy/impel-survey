5f01e703aa255b3884c74a1c8ce4c2f3c952ef4b
Mohammad Jakir Hossain <mjakir.ddd@gmail.com>
2021-08-02 23:31:14 +0600

    Add view for survey list

--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Survey
+
+
+class IndexView(TemplateView):
+    template_name = "survey/list.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        surveys = Survey.objects.all()
+        context["surveys"] = surveys
+        return context
