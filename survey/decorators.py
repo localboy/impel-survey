831633e67c91b1a16a8db45579f848ddd456a79c
Mohammad Jakir Hossain <mjakir.ddd@gmail.com>
2021-08-04 22:02:03 +0600

    Updated valid_survey decorator to check if user already participated

--- survey/decorators.py
+++ survey/decorators.py
@@ -2,7 +2,7 @@ import logging
 from datetime import date
 from functools import wraps
 
-from django.shortcuts import Http404, get_object_or_404
+from django.shortcuts import Http404, get_object_or_404, redirect, reverse
 
 from survey.models import Survey
 
@@ -14,8 +14,18 @@ def valid_survey(func):
 
     @wraps(func)
     def survey_check(self, request, *args, **kwargs):
+        session_key = 'survey_{}_{}'.format(request.user.id, kwargs['id'])
         survey = get_object_or_404(
-            Survey.objects.prefetch_related("questions"), id=kwargs["id"])
+            Survey.objects.prefetch_related("questions", "responses"), id=kwargs["id"])
+        
+        # checking if there any existing response
+        if survey.responses.filter(user = request.user).count()>0:
+            return redirect(reverse("survey-participated"))
+
+        # Once user start the survey, whether he can submit or not,
+        # he will not able to participate again.
+        if session_key in request.session:
+            return redirect(reverse("survey-participated"))
         return func(self, request, *args, **kwargs, survey=survey)
 
     return survey_check
