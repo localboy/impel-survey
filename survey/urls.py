35df9afd192259b140f0c6ca8af78bfe5f888512
Mohammad Jakir Hossain <mjakir.ddd@gmail.com>
2021-08-04 10:46:56 +0600

    Add survey instruction view, url and template

--- survey/urls.py
+++ survey/urls.py
@@ -1,8 +1,9 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, SurveyInstruction
 
 urlpatterns = [
     path('', staff_member_required(IndexView.as_view()), name='survey-list'),
+    path('survey/<id>/instructions/', SurveyInstruction.as_view(), name='survey-instructions'),
 ]
--- survey/views.py
+++ survey/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.shortcuts import redirect, render, reverse
 
+from survey.decorators import survey_available
+from .forms import ResponseForm
 from .models import Survey
 
 
@@ -10,4 +13,13 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         surveys = Survey.objects.all()
         context["surveys"] = surveys
+        return context
+
+
+class SurveyInstruction(TemplateView):
+    template_name = 'survey/instruction.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['survey'] = Survey.objects.get(id=kwargs['id'])
         return context
