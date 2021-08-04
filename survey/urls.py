1a667bde4ed413b39f84d2039e8b4bc1b0e41282
Mohammad Jakir Hossain <mjakir.ddd@gmail.com>
2021-08-04 12:20:45 +0600

    Urls for survey details

--- survey/urls.py
+++ survey/urls.py
@@ -1,9 +1,11 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path
 
-from .views import IndexView, SurveyInstruction
+from .views import IndexView, SurveyInstruction, SurveyDetail
 
 urlpatterns = [
     path('', staff_member_required(IndexView.as_view()), name='survey-list'),
     path('survey/<id>/instructions/', SurveyInstruction.as_view(), name='survey-instructions'),
+    path('survey/<id>/', SurveyDetail.as_view(), name='survey-detail'),
+    path('<id>-<step>/', SurveyDetail.as_view(), name="survey-detail-step"),
 ]
